7a0f8688a2f47604b5627a60b36b7f32a2a08502
hidao80 <8155294+hidao80@users.noreply.github.com>
2020-07-12 05:22:00 +0900

    doc: コメントを追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+/**
+ * getElementByIdのショートカット
+ * @param {string} id - idに指定している文字列
+ */
 function $(id) {
     return document.getElementById(id);
 }
 
+/**
+ * セル内でダブルクリックしたときのコールバック関数
+ * @param {Event} e - ダブルクリック時のイベントオブジェクト 
+ */
 function dblclickAction (e) {
+    // バッジのオンオフを行う
     let target = e.currentTarget;
     if (target.dataset.done === undefined) {
         target = target.lastChild;
@@ -10,6 +19,11 @@ function dblclickAction (e) {
     setBadge(target, target.dataset.done === "true" ? "false" : "true");
 }
 
+/**
+ * バッジのオンオフを切り替える
+ * @param {Element} e - dataset.doneを持つElement
+ * @param {string} b - booleanを表す文字列
+ */
 function setBadge(e, b) {
     if (b === "true") {
         e.previousElementSibling.innerText = "⭐";
@@ -20,9 +34,15 @@ function setBadge(e, b) {
     }
 }
 
+/**
+ * 編集中のマンダラートを保存する
+ * @param {Event} e - 保存ボタンをクリックしたときのイベントオブジェクト
+ */
 function save(e) {
-    // 画面の内容を localStroage に保存
+    // セルとなるDOMをリストアップ
     const cells = document.querySelectorAll(".cell");
+
+    // セルのID、完了バッジの状態、目標のデータをJavascriptオブジェクト化
     const jso = [...cells].map((el) => {
         const child = el.lastChild;
         return {
@@ -31,6 +51,8 @@ function save(e) {
             text: child.innerText
         };
     });
+
+    // 画面の内容をJSONに変換し、URLにつけたハッシュをセーブスロットとしてlocalStroageに保存
     localStorage.setItem(location.hash, JSON.stringify(jso));
 
     // 保存時アニメーション
@@ -40,12 +62,18 @@ function save(e) {
     }, 3000);
 }
 
+/**
+ * 前回保存したマンダラートを読み込む
+ * @param {string} json - 全開保存したマンダラートを表すJSON
+ */
 function load(json) {
     // 保存データがないときは処理しない
     if (json === null) return;
 
+    // JSONをjavascriptオブジェクトに展開
     const jso = JSON.parse(json);
 
+    // JSONの内容を各セルに書き込む
     jso.forEach(e => {
         const elem = $(e.id);
         setBadge(elem, e.done);
@@ -53,6 +81,10 @@ function load(json) {
     });
 }
 
+/**
+ * DOM読み込み完了時の動作
+ * @param {function} loaded - DOM読み込み完了時に実行するコールバック関数
+ */
 function ready(loaded) {
 	if (["interactive", "complete"].includes(document.readyState)) {
 		loaded();
@@ -61,14 +93,20 @@ function ready(loaded) {
 	}
 }
 
+// DOMの読み込みより先にJavascriptが呼び出されても大丈夫なように、読み込み完了時に
+// 動作する初期化処理をコールバック関数としてセットしておく
 ready(() => {
+    // セルとなるDOMをリストアップ    
     const cells = document.querySelectorAll(":scope .cell");
 
+    // すべてのセルにダブルクリック時のコールバック関数を登録する
     cells.forEach(cell => {
         cell.addEventListener("dblclick", e => dblclickAction(e));
     });
+
+    // 保存ボタンをクリック時のコールバック関数を登録する
     $("save").addEventListener("click", e => save(e));
 
-    // 保存データのロード
+    // 保存データのロードを試みる
     load(localStorage.getItem(location.hash));
 });
